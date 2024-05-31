3846d681c97d54e87114a4a07f634c18a44f18cf
Ken Leung <kenleung5e28@gmail.com>
2024-06-01 00:05:35 +0800

    section 4.3

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,16 +1,17 @@
 package services;
 
 import models.Comment;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
-@Component
+@Service
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
